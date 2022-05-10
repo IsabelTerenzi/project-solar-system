138259311369e2c4c796a7a11a7577e3d826b146
Isabel Terenzi <bel.terenzi@gmail.com>
2022-05-10 14:17:26 -0300

    organiza css

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -5,12 +5,14 @@ class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{name}</p>
-        <p data-testid="mission-year">{year}</p>
-        <p data-testid="mission-country">{country}</p>
-        <p data-testid="mission-destination">{destination}</p>
-      </div>
+      <article>
+        <div data-testid="mission-card">
+          <h3 data-testid="mission-name">{name}</h3>
+          <h4 data-testid="mission-year">{year}</h4>
+          <h4 data-testid="mission-country">{country}</h4>
+          <h4 data-testid="mission-destination">{destination}</h4>
+        </div>
+      </article>
     );
   }
 }
